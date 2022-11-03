69211a53081e33613e76805bedb19ba82f7975a2
Woong99 <dndry1072@naver.com>
2022-11-03 21:43:42 +0900

    [Feat] : 프로젝트 생성(#2)
    SpringBoot 프로젝트를 생성하고, CRA를 통해 React 프로젝트 생성함

---
+++ Sic9/src/main/java/com/example/potato/sic9/Sic9Application.java
@@ -0,0 +1,13 @@
+package com.example.potato.sic9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sic9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sic9Application.class, args);
+    }
+
+}
---
+++ Sic9/src/test/java/com/example/potato/sic9/Sic9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.potato.sic9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sic9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
