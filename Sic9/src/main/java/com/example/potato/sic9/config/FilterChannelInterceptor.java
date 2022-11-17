07fa1864665f7a9161de348021032fa9ef452f97
Woong99 <dndry1072@naver.com>
2022-11-17 21:14:53 +0900

    [Feat] : JWT 토큰을 받아 사용자 인증을 추가하는 인터셉터 추가(#11)

--- Sic9/src/main/java/com/example/potato/sic9/config/FilterChannelInterceptor.java
+++ Sic9/src/main/java/com/example/potato/sic9/config/FilterChannelInterceptor.java
@@ -1,6 +1,7 @@
 package com.example.potato.sic9.config;
 
 import com.example.potato.sic9.jwt.JwtTokenProvider;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -10,14 +11,14 @@ import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 @RequiredArgsConstructor
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class FilterChannelInterceptor implements ChannelInterceptor {
+
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
@@ -25,8 +26,16 @@ public class FilterChannelInterceptor implements ChannelInterceptor {
         StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
         assert headerAccessor != null;
-        if (headerAccessor.getCommand() == StompCommand.CONNECT) {
-            jwtTokenProvider.validateToken(Objects.requireNonNull(headerAccessor.getNativeHeader("Authorization")).get(0));
+        if (StompCommand.CONNECT.equals(headerAccessor.getCommand())) {
+
+            String token = Objects.requireNonNull(headerAccessor.getNativeHeader("Authorization")).get(0)
+                    .replace("Bearer ", "");
+            try {
+                Authentication user = jwtTokenProvider.getAuthentication(token);
+                headerAccessor.setUser(user);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return message;
     }
