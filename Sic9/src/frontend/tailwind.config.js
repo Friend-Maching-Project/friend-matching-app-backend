fde017b4dd0eef6372cfb4a507fa2c02fd706094
Woong99 <dndry1072@naver.com>
2022-11-19 16:02:20 +0900

    [Feat] : 로고 이미지 사용을 위해 설정 추가(#13)

--- Sic9/src/frontend/tailwind.config.js
+++ Sic9/src/frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'google-logo': "url('../public/assets/google_logo.png')",
+        'naver-logo': "url('../public/assets/naver_logo.png')",
+        'kakao-logo': "url('../public/assets/kakao_logo.png')",
+      },
+    },
   },
   plugins: [],
 };
