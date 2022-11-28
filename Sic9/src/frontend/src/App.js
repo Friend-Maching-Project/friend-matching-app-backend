9c9ab1ea13044cfc21f5ad375a9e76c99d8e7197
Woong99 <dndry1072@naver.com>
2022-11-28 17:27:13 +0900

    [Feat] : 로그인, 회원가입 페이지 라우팅 연결, 모바일에 맞는 높이 설정(#21)

--- Sic9/src/frontend/src/App.js
+++ Sic9/src/frontend/src/App.js
@@ -1,11 +1,18 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
