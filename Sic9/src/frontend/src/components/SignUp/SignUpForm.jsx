448d8d31be4d14153ad1e46cf64b9826d23092ba
Woong99 <dndry1072@naver.com>
2022-11-28 17:32:51 +0900

    [Chore] : 주석 제거(#21)

--- Sic9/src/frontend/src/components/SignUp/SignUpForm.jsx
+++ Sic9/src/frontend/src/components/SignUp/SignUpForm.jsx
@@ -8,18 +8,6 @@ const SignUpForm = ({ children, page }) => {
         <div className="w-full">
           <div className="text-waniGreen font-bold text-4xl mb-12">Register</div>
           {children}
-          {/* <div>
-            {page !== 0 && (
-              <button className="mt-12 float-left" onClick={goPreviousPage}>
-                <FontAwesomeIcon icon={faCircleLeft} className="text-waniGreen text-3xl" />
-              </button>
-            )}
-            {page !== 5 && (
-              <button className="mt-12 float-right" onClick={goNextPage}>
-                <FontAwesomeIcon icon={faCircleRight} className="text-waniGreen text-3xl" />
-              </button>
-            )}
-          </div> */}
         </div>
       </div>
       <div className="absolute bottom-10 left-1/2 translate-x-[-50%]">{page + 1}/6</div>
