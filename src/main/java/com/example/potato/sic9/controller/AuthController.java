6420c80592a7861e38f6a8970734af49f62fa9f0
Woong99 <dndry1072@naver.com>
2022-12-23 14:43:34 +0900

    [Refactor] : Cookie Util 클래스를 이용하여 코드의 중복 제거(#27)

--- src/main/java/com/example/potato/sic9/controller/AuthController.java
+++ src/main/java/com/example/potato/sic9/controller/AuthController.java
@@ -1,18 +1,18 @@
 package com.example.potato.sic9.controller;
 
+import com.example.potato.sic9.common.ExpireTime;
+import com.example.potato.sic9.dto.auth.AccessTokenDto;
 import com.example.potato.sic9.dto.auth.LoginRequestDto;
 import com.example.potato.sic9.dto.auth.RefreshTokenDto;
 import com.example.potato.sic9.dto.auth.SignUpRequestDto;
-import com.example.potato.sic9.dto.auth.TokenDto;
-import com.example.potato.sic9.jwt.JwtTokenProvider;
+import com.example.potato.sic9.security.jwt.JwtTokenProvider;
 import com.example.potato.sic9.service.AuthService;
 import com.example.potato.sic9.service.RefreshTokenService;
+import com.example.potato.sic9.util.CookieUtil;
 import java.util.Date;
 import java.util.Map;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -26,12 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@Slf4j
 public class AuthController {
     private final AuthService authService;
     private final RefreshTokenService refreshTokenService;
     private final JwtTokenProvider jwtTokenProvider;
-    private static final int COOKIE_EXPIRE_TIME = 60 * 60 * 10;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@RequestBody SignUpRequestDto requestDto) throws Exception {
@@ -56,19 +54,21 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenDto> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
+    public ResponseEntity<AccessTokenDto> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
         Map<String, Object> tokens = authService.login(requestDto);
-        TokenDto accessToken = (TokenDto) tokens.get("accessToken");
+        AccessTokenDto accessToken = (AccessTokenDto) tokens.get("accessToken");
         RefreshTokenDto refreshTokenDto = (RefreshTokenDto) tokens.get("refreshToken");
         String name = jwtTokenProvider.getUserName(accessToken.getToken());
         refreshTokenService.saveRefreshToken(name, refreshTokenDto);
-        response.addCookie(generateCookieRefreshToken(refreshTokenDto));
+        String refreshToken = refreshTokenDto.getRefreshToken();
+        CookieUtil.addCookie(response, "refreshToken", refreshToken,
+                (int) ExpireTime.REFRESH_COOKIE_EXPIRE_TIME.getTime());
 
         return ResponseEntity.ok(accessToken);
     }
 
     @GetMapping("/refresh")
-    public ResponseEntity<TokenDto> refreshToken(
+    public ResponseEntity<AccessTokenDto> refreshToken(
             @CookieValue(value = "refreshToken", required = false) String cookieRefreshToken,
             HttpServletResponse response) {
         String username = null;
@@ -87,7 +87,8 @@ public class AuthController {
                 refreshToken = newRefreshTokenDto.getRefreshToken();
                 if (jwtTokenProvider.validateToken(refreshToken)) {
                     refreshTokenService.saveRefreshToken(username, newRefreshTokenDto);
-                    response.addCookie(generateCookieRefreshToken(newRefreshTokenDto));
+                    CookieUtil.addCookie(response, "refreshToken", refreshToken,
+                            (int) ExpireTime.REFRESH_COOKIE_EXPIRE_TIME.getTime());
                 } else {
                     // TODO : 예외 처리
                 }
@@ -95,16 +96,5 @@ public class AuthController {
 
         }
         return ResponseEntity.ok(jwtTokenProvider.generateAccessTokenDto(username));
-
     }
-
-    private Cookie generateCookieRefreshToken(RefreshTokenDto dto) {
-        // TODO : secure 설정하기
-        Cookie cookie = new Cookie("refreshToken", dto.getRefreshToken());
-        cookie.setPath("/");
-        cookie.setMaxAge(COOKIE_EXPIRE_TIME);
-        cookie.setHttpOnly(true);
-        return cookie;
-    }
-
 }
